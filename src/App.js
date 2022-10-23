63d4fcc65cd274a6f050a744a5bd4fae69850f9b
özkan demir <ozkanndemir2009@gmail.com>
2022-10-24 00:00:30 +0300

    css added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ const url = "https://course-api.com/react-tabs-project";
 function App() {
   const [getuser, setGetuser] = useState("");
   const [change, setChange] = useState([]);
-  const [duti, setDuti] = useState([])
+  const [duti, setDuti] = useState([]);
 
   const getApi = async () => {
     try {
@@ -29,58 +29,57 @@ function App() {
 
   console.log(getuser);
 
-
   return (
-    <div className="container text-center ">
-      <h1>Experience</h1>
-      <div className="button">
-        <button onClick={() => setChange([duties,change, getuser[0]])}>TOOMY</button>
-        <button onClick={() => setChange([duties,change, getuser[1]])}>BIGDROP</button>
-        <button onClick={() => setChange([change, getuser[2]])}>CUKER</button>
+    <section className="section">
+      <div className="title">
+        <h1>Experience</h1>
+        <div className="underline"></div>
+        <div className="jobs-center">
+          <div className="btn-container">
+            <button
+              className="btn1"
+              onClick={() => setChange([duties, change, getuser[0]])}
+            >
+              TOOMY
+            </button>
+            <button
+              className="btn1"
+              onClick={() => setChange([duties, change, getuser[1]])}
+            >
+              BIGDROP
+            </button>
+            <button
+              className="btn1"
+              onClick={() => setChange([change, getuser[2]])}
+            >
+              CUKER
+            </button>
+          </div>
+        </div>
+        <article className="job-info">
+          {change.map((item, index) => {
+            return (
+              <div key={index}>
+                <p>{item?.title}</p>
+                <p> {item?.company} </p>
+                <p> {item?.dates} </p>
+                {[item?.duties].map((duti) => {
+                  return (
+                    <div className="job-desc">
+                      <div>
+                        <p>{duti?.[0]}</p>
+                        <p>{duti?.[1]}</p>
+                        <p>{duti?.[2]}</p>
+                      </div>
+                    </div>
+                  );
+                })}{" "}
+              </div>
+            );
+          })}
+        </article>
       </div>
-      <article>
-        {change.map((item,index)=>{
-          return(
-            <div key={index} >
-            <p>{item?.title}</p>
-            <p> {item?.company}  </p>
-            <p> {item?.dates} </p>  
-            <p>  {[item?.duties].map((duti)=>{
-              return(
-                <div>
-                  <p>{duti?.[0]}</p> 
-                  <p>{duti?.[1]}</p> 
-                  <p>{duti?.[2]}</p> 
-
-                  </div>
-              )
-            })} </p>
-             
-            </div>
-          )
-        })}
-
-      </article>
- 
- 
- 
- 
- 
- 
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
-
-      <button>MORE INFO</button>
-    </div>
+    </section>
   );
 }
 
