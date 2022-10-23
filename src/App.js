cac269d7e727c8a92c2057de639752881240b229
özkan demir <ozkanndemir2009@gmail.com>
2022-10-23 23:24:14 +0300

    app.js finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,87 @@
-import React, { useState, useEffect } from 'react'
-import { FaAngleDoubleRight } from 'react-icons/fa'
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tabs-project'
+const url = "https://course-api.com/react-tabs-project";
 function App() {
-  return <h2>tabs project setup</h2>
+  const [getuser, setGetuser] = useState("");
+  const [change, setChange] = useState([]);
+  const [duti, setDuti] = useState([])
+
+  const getApi = async () => {
+    try {
+      const { data } = await axios(url);
+
+      setGetuser(data);
+      console.log(data);
+      // console.log(data[0]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getApi();
+  }, []);
+
+  const { title, company, dates, duties } = getuser;
+
+  console.log(getuser);
+
+
+  return (
+    <div className="container text-center ">
+      <h1>Experience</h1>
+      <div className="button">
+        <button onClick={() => setChange([duties,change, getuser[0]])}>TOOMY</button>
+        <button onClick={() => setChange([duties,change, getuser[1]])}>BIGDROP</button>
+        <button onClick={() => setChange([change, getuser[2]])}>CUKER</button>
+      </div>
+      <article>
+        {change.map((item,index)=>{
+          return(
+            <div key={index} >
+            <p>{item?.title}</p>
+            <p> {item?.company}  </p>
+            <p> {item?.dates} </p>  
+            <p>  {[item?.duties].map((duti)=>{
+              return(
+                <div>
+                  <p>{duti?.[0]}</p> 
+                  <p>{duti?.[1]}</p> 
+                  <p>{duti?.[2]}</p> 
+
+                  </div>
+              )
+            })} </p>
+             
+            </div>
+          )
+        })}
+
+      </article>
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+      <button>MORE INFO</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
